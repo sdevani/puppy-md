fe55a059dd556ff92ad2e3d061df9cd9f8638c75
Mazdak Momen <mazdak.momen@gmail.com>
2014-12-09 17:29:29 -0800

    Purchases requests can be created and retrieved in purchaserequestrepo, purchaserequest can be used to created the purcaserequest class object

--- lib/puppy_breeder/purchase_request.rb
+++ lib/puppy_breeder/purchase_request.rb
@@ -1,6 +1,26 @@
 #Refer to this class as PuppyBreeder::PurchaseRequest
-module PuppyBreeder
+#purchase request for customer
+#customer will pick breed, need to keep customer's name
+# module PuppyBreeder
   class PurchaseRequest
-
+    attr_reader :customer_name, :breed
+  
+    def initialize(customer_name, breed)
+      @customer_name = customer_name
+      @breed = breed
+    end
   end
-end
+
+  class PurchaseRequestRepo
+    @request = Hash.new
+  
+    def self.create(customer_name, breed)
+      pr = PurchaseRequest.new(customer_name, breed)
+      @request[customer_name] = pr
+    end
+
+    def self.get(customer_name)
+      @request[customer_name]
+    end
+  end
+# end
--- spec/purchase_request_spec.rb
+++ spec/purchase_request_spec.rb
@@ -1,5 +1,18 @@
 require_relative 'spec_helper.rb'
 
-describe PuppyBreeder::PurchaseRequest do
-
+describe PurchaseRequestRepo do
+  describe ".create" do
+    it "creates a purchase request" do
+      result = PurchaseRequestRepo.create('Bob', 'Terrier')
+      expect(result).to be_a(PurchaseRequest)
+    end
+  end
+  describe ".get" do
+    it "gets the request purchase request" do
+      PurchaseRequestRepo.create('Bob', 'Terrier')
+      result = PurchaseRequestRepo.get('Bob')
+      expect(result.customer_name).to eq('Bob')
+      expect(result.breed).to eq('Terrier')
+    end
+  end
 end
